0d470f625c6eacfdbb8bd4eae86d4cb5a169b11a
Nazmun Nahar <nnahar@ggc.edu>
2023-02-03 00:06:09 +0600

    Initial commit

---
+++ src/SpeedLight.java
@@ -0,0 +1,14 @@
+/**
+ * @Class Name: SpeedLight
+ * @Author: Nazmun Nahar
+ * @Version: 1.0
+ * @Date: 2 February 2023
+ * Description: A program designed to calculate the speed of light
+ */
+
+
+public class SpeedLight {
+    public static void main(String[] args){
+
+    }
+}
