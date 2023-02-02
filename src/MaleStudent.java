49e280dec89559ed1cbb21afe3bcbca7e1ad3318
Nazmun Nahar <nnahar@ggc.edu>
2023-02-03 02:09:38 +0600

    Adding code in MaleStudent

--- src/MaleStudent.java
+++ src/MaleStudent.java
@@ -1,2 +1,17 @@
+/**
+ * @Class Name: MaleStudent
+ * @Author:Nazmun Nahar
+ * @Version:1.0
+ * @Date:2 February 2023
+ * Description:A program designed to calculate the number of Male students
+ */
+
 public class MaleStudent {
+    public static void main(String[] args){
+        int numberOfStudents = 389;
+        int numberOfFemaleStudents = 175;
+        int nuberOfMaleStudents = numberOfStudents - numberOfFemaleStudents;
+
+        System.out.println("Number of Male students: " + nuberOfMaleStudents);
+    }
 }
