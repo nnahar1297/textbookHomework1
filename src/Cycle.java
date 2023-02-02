57741c1ab85e86b230b2dfc25c603c22d6df27c6
Nazmun Nahar <nnahar@ggc.edu>
2023-02-03 01:53:08 +0600

    Adding code in the Cycle

--- src/Cycle.java
+++ src/Cycle.java
@@ -1,2 +1,11 @@
 public class Cycle {
+    public static void main(String[] args){
+        int numberOfWheel = 47;
+        int numberOfBicycle = 10;
+        int BICYCLE_WHEEL = numberOfBicycle * 2;
+        int TRYCYCLE_WHEEL = numberOfWheel - BICYCLE_WHEEL;
+        int numberOfTrycycle = TRYCYCLE_WHEEL/3;
+
+        System.out.println("numberOfTrycycle: " + numberOfTrycycle);
+    }
 }
