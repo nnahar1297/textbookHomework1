1195298eb0269ce1abb25fcc300a5a4669c50c8f
Nazmun Nahar <nnahar@ggc.edu>
2023-02-03 02:04:33 +0600

    Finding X's value

--- src/FindX.java
+++ src/FindX.java
@@ -1,2 +1,17 @@
+/**
+ * @Class Name: FindX
+ * @Author:Nazmun Nahar
+ * @Version:1.0
+ * @Date:2 February 2023
+ * Description:A program designed to calculate the X
+ */
 public class FindX {
+    public static void main(String[] args){
+        int X;
+        int otherX = 5+19+47; //Based on the formula given 5 + 19 + X + 47 = 194
+
+        X = 194 - otherX;
+
+        System.out.println("X: " + X);
+    }
 }
