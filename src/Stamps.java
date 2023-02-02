9f6ae05c068065aa7249f0f2af58db3df2edddc2
Nazmun Nahar <nnahar@ggc.edu>
2023-02-03 01:35:07 +0600

    Adding code in the Stamps

--- src/Stamps.java
+++ src/Stamps.java
@@ -1,2 +1,20 @@
+/**
+ * @Class Name: Stamps
+ * @Author:Nazmun Nahar
+ * @Version:1.0
+ * @Date:2 February 2023
+ * Description:A program designed to calculate the difference between the
+ * collections of Jean and Susan
+ */
+
 public class Stamps {
+    public static void main(String[] args){
+        int SUSAN_STAMPS = 8;
+        int JEAN_STAMPS = 40;
+        int difference = JEAN_STAMPS - SUSAN_STAMPS;
+
+        System.out.println("SUSAN_STAMPS: " + SUSAN_STAMPS);
+        System.out.println("JEAN_STAMPS: " + JEAN_STAMPS);
+        System.out.println("Jean has " + difference + " stamps more than Susan");
+    }
 }
