c2629da9564639d5f826a9faad1e4dc23fa30d4b
Nazmun Nahar <nnahar@ggc.edu>
2023-02-03 01:18:49 +0600

    Adding code in the Chocolate

--- src/Chocolate.java
+++ src/Chocolate.java
@@ -3,9 +3,18 @@
  * @Author: Nazmun Nahar
  * @Version: 1.0
  * @Date: 2 February 2023
- * Description: A program designed to calculate the speed of light
+ * Description: A program designed to calculate the minimum number of bags
  */
 
 
 public class Chocolate {
+    public static void main(String[] args){
+        int ChocolateBag = 9;
+        int ChocolateBar = ChocolateBag * 2;
+        int minimumBag = ChocolateBar/3;
+
+        System.out.println("ChocolateBag: " + ChocolateBag);
+        System.out.println("ChocolateBar: " + ChocolateBar);
+        System.out.println("ChocolateBag needed: " + minimumBag);
+    }
 }
