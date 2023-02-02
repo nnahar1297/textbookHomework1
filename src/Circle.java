21bed92c6d4442c4cfc24a0b6dac3bbf219aea54
Nazmun Nahar <nnahar@ggc.edu>
2023-02-03 02:24:19 +0600

    Adding code in Circle

--- src/Circle.java
+++ src/Circle.java
@@ -1,2 +1,17 @@
+/**
+ * @Class Name: Circle
+ * @Author: Nazmun Nahar
+ * @Version: 1.0
+ * @Date:2 February 2023
+ * Description:A program designed to calculate area and perimeter
+ */
 public class Circle {
+    public static void main(String[] args){
+        double r = 9.5;
+        double area = r * r * Math.PI;
+        double perimeter = 2 * r * Math.PI;
+
+        System.out.println("Area: " + area);
+        System.out.println("Perimeter: " + perimeter);
+    }
 }
